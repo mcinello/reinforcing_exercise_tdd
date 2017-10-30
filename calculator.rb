acebaef2e9d0d116f9c2c341ed17839734b33fe0
Michelle Cinello <m.cinello@gmail.com>
2017-10-30 10:29:02 -0400

    test_add_positive_numbers works

---
+++ calculator.rb
@@ -0,0 +1,8 @@
+class Calculator
+
+  def add(n1, n2)
+    return n1 + n2
+  end
+
+
+end
--- test_calculator.rb
+++ test_calculator.rb
@@ -5,38 +5,54 @@ require './calculator.rb'
 
 class TestCalculator < MiniTest::Test
   # add takes two parameters and adds them
+
+  def setup
+    @calculator = Calculator.new
+  end
+
   def test_adds_0_and_0
+    skip
     assert_equal 0, add(0, 0)
   end
 
   def test_adds_2_and_2
+    skip
     assert_equal 4, add(2, 2)
   end
 
   def test_adds_positive_numbers
-    assert_equal 8, add(2, 6)
+    result = @calculator.add(2,2)
+
+    expected = 4
+
+    assert_equal expected, result
   end
 
  # subtract takes two parameters and subtracts the second from the first
   def test_subtracts_numbers
+    skip
     assert_equal 6, subtract(10, 4)
   end
 
 # sum takes an *array* of numbers and adds them all together
 # This one is a bit trickier!
   def test_computes_sum_of_empty_array
+    skip
     assert_equal(0, sum([]))
   end
 
   def test_computes_sum_of_single_number
+    skip
     assert_equal 7, sum([7])
   end
 
   def test_computes_sum_of_two_numbers
+    skip
     assert_equal 18, sum([7, 11])
   end
 
   def test_computes_sum_of_many_numbers
+    skip
     assert_equal 25, sum([1,3,5,7,9])
   end
 end
